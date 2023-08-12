7a2006d19ce0ca11577f807a3ec30b382017e96f
Christine Baik <christuaik@gmail.com>
2023-08-12 15:10:22 -0700

    complete

--- lib/dog.py
+++ lib/dog.py
@@ -12,4 +12,13 @@ APPROVED_BREEDS = [
 ]
 
 class Dog:
-    pass
+    def __init__(self, name = 'Fido', breed = 'Mastiff'):
+        self.name = name
+        if (type(name) == str and len(name) > 1 and len(name) < 25):
+            pass
+        else:
+            print('Name must be string between 1 and 25 characters.')
+        
+        self.breed = breed
+        if (len(breed) > 0 and breed not in APPROVED_BREEDS):
+            print('Breed must be in list of approved breeds.')
--- lib/person.py
+++ lib/person.py
@@ -16,4 +16,11 @@ APPROVED_JOBS = [
 ]
 
 class Person:
-    pass
+    def __init__(self, name = 'Guido', job = "Sales"):
+        if (type(name) == str and len(name) > 0 and len(name) < 26):
+            self.name = name.title()
+        else:
+            print('Name must be string between 1 and 25 characters.')
+        self.job = job
+        if (len(job) > 0 and job not in APPROVED_JOBS):
+            print('Job must be in list of approved jobs.')
